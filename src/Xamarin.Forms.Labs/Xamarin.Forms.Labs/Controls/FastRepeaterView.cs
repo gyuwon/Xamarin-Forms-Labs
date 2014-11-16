d560dbb1cd0d3f8a533e0c0318375a7a9efa8b23
Gyuwon Yi <gyuwon@tidcorp.com>
2014-11-17 01:21:00 +0900

    simpler and faster

--- src/Xamarin.Forms.Labs/Xamarin.Forms.Labs/Controls/FastRepeaterView.cs
+++ src/Xamarin.Forms.Labs/Xamarin.Forms.Labs/Controls/FastRepeaterView.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
 
@@ -6,31 +8,14 @@ namespace Xamarin.Forms.Labs.Controls
 {
     public class FastRepeaterView<T> : RepeaterView<T>
     {
-        private class Measure
-        {
-            public Size? Size { get; set; }
-
-            public Size? Offset { get; set; }
-
-            public void MakeDirty(bool size = true, bool offset = true)
-            {
-                if (size)
-                {
-                    Size = null;
-                }
-                if (offset)
-                {
-                    Offset = null;
-                }
-            }
-        }
-
-        private static BindableProperty MeasureProperty = BindableProperty.CreateAttached("Measure", typeof(Measure), typeof(FastRepeaterView<T>), null);
-
-        private Rectangle? _lastBounds;
+        private BitArray _layoutKey;
+        private int _layoutCompletion;
 
         public FastRepeaterView()
         {
+            _layoutKey = null;
+            _layoutCompletion = 0;
+
             ((INotifyCollectionChanged)Children).CollectionChanged += OnElementCollectionChanged;
         }
 
@@ -41,8 +26,8 @@ namespace Xamarin.Forms.Labs.Controls
                 foreach (View item in e.OldItems)
                 {
                     item.MeasureInvalidated -= OnChildMeasureInvalidated;
-                    item.SetValue(MeasureProperty, null);
                 }
+                MakeLayoutDirtyFrom(e.OldStartingIndex);
             }
 
             if (e.NewItems != null)
@@ -51,145 +36,125 @@ namespace Xamarin.Forms.Labs.Controls
                 {
                     item.MeasureInvalidated += OnChildMeasureInvalidated;
                 }
+                MakeLayoutDirtyFrom(e.NewStartingIndex);
             }
+        }
+
+        private void MakeLayoutDirtyFrom(int index)
+        {
+            _layoutCompletion = Math.Min(_layoutCompletion, index);
+        }
+
+        private void MakeLayoutDirtyFrom(View view)
+        {
+            MakeLayoutDirtyFrom(Children.IndexOf(view));
         }
 
         new private void OnChildMeasureInvalidated(object sender, EventArgs e)
         {
-            foreach (var view in Children.Skip(Children.IndexOf((View)sender)))
-            {
-                view.GetValue<Measure>(MeasureProperty).MakeDirty(size: sender == view);
-            }
+            MakeLayoutDirtyFrom((View)sender);
         }
 
-        protected override void LayoutChildren(double x, double y, double width, double height)
+        private void EnsureLayout(Rectangle bounds)
         {
-            var bounds = new Rectangle(x, y, width, height);
-
-            if (_lastBounds != bounds)
-            {
-                foreach (var child in Children)
-                {
-                    var measure = child.GetValue<Measure>(MeasureProperty);
-                    if (measure != null)
-                    {
-                        measure.MakeDirty();
-                    }
-                }
-            }
-
-            EnsureChildrenMeasure(bounds);
-
-            foreach (var view in Children)
-            {
-                var measure = view.GetValue<Measure>(MeasureProperty);
-                LayoutChild(view, new Rectangle
-                                  {
-                                      Size = measure.Size.Value,
-                                      Location = (Point)measure.Offset.Value
-                                  });
-            }
-
-            _lastBounds = bounds;
-        }
-
-        private void LayoutChild(View view, Rectangle layoutBounds)
-        {
-            if (view.Bounds == layoutBounds)
-            {
-                return;
-            }
-
-            view.Layout(layoutBounds);
-        }
-
-        private void EnsureChildrenMeasure(Rectangle bounds)
-        {
-            if (Children.Any() == false)
-            {
-                return;
-            }
-
-            EnsureChildrenSize(bounds.Size);
-
-            double offset = 0.0;
-            foreach (var view in Children)
-            {
-                var measure = view.GetValue<Measure>(MeasureProperty);
-                measure.Offset = new Size
-                                 {
-                                     Width = bounds.Left,
-                                     Height = offset
-                                 };
-                offset += (measure.Size.Value.Height + Spacing);
-            }
-        }
-
-        private void EnsureChildrenSize(Size size)
-        {
-            if (Children.Any() == false)
-            {
-                return;
-            }
-
-            foreach (var view in Children)
-            {
-                var measure = view.GetValue<Measure>(MeasureProperty);
-                if (measure == null)
-                {
-                    measure = new Measure();
-                    view.SetValue(MeasureProperty, measure);
-                }
-                if (measure.Size == null)
-                {
-                    if (view.IsVisible == false)
-                    {
-                        measure.Size = Size.Zero;
-                    }
-                    else
-                    {
-                        var sizeRequest = view.GetSizeRequest(size.Width, size.Height);
-                        measure.Size = sizeRequest.Request;
-                    }
-                }
-            }
-        }
-
-        protected override SizeRequest OnSizeRequest(double widthConstraint,
-                                                     double heightConstraint)
-        {
-            if (Children.Any() == false)
-            {
-                return new SizeRequest
-                       {
-                           Request = Size.Zero,
-                           Minimum = Size.Zero
-                       };
-            }
-
             switch (Orientation)
             {
                 case StackOrientation.Horizontal:
                     throw new NotImplementedException();
 
                 case StackOrientation.Vertical:
-                    return OnSizeRequestVertical(widthConstraint, heightConstraint);
+                    EnsureLayoutVertical(bounds);
+                    break;
 
                 default:
                     throw new InvalidOperationException();
             }
         }
 
-        private SizeRequest OnSizeRequestVertical(double widthConstraint, double heightConstraint)
+        private void EnsureLayoutVertical(Rectangle bounds)
         {
-            EnsureChildrenSize(new Size { Width = widthConstraint, Height = heightConstraint });
+            var completion = _layoutCompletion;
+            var key = CreateKey(bounds.Left, bounds.Top, bounds.Width);
+            if (_layoutKey == null || new BitArray(_layoutKey).Xor(key).Cast<bool>().Any(e => e))
+            {
+                completion = 0;
+            }
+            for (; completion < Children.Count; completion++)
+            {
+                var view = Children[completion];
+                var sizeRequest = view.GetSizeRequest(bounds.Width, bounds.Height);
+                var offset = 0.0;
+                if (completion != 0)
+                {
+                    offset = Children[completion - 1].Bounds.Bottom + Spacing;
+                }
+                view.Layout(new Rectangle
+                            {
+                                Location = new Point(0, offset),
+                                Size = new Size
+                                       {
+                                           Width = bounds.Width,
+                                           Height = sizeRequest.Request.Height
+                                       }
+                            });
+            }
+            _layoutKey = key;
+            _layoutCompletion = completion;
+        }
 
-            var measures = Children.Select(v => v.GetValue<Measure>(MeasureProperty));
-            var width = measures.Max(m => m.Size.Value.Width);
-            var height = measures.Aggregate(seed: -Spacing,
-                                            func: (a, m) => a + m.Size.Value.Height + Spacing);
-            var request = new Size { Width = width, Height = height };
-            return new SizeRequest { Request = request, Minimum = request };
+        protected override SizeRequest OnSizeRequest(double widthConstraint,
+                                                     double heightConstraint)
+        {
+            EnsureLayout(new Rectangle
+                         {
+                             Location = Point.Zero,
+                             Size = new Size
+                                    {
+                                        Width = widthConstraint,
+                                        Height = heightConstraint
+                                    }
+                         });
+
+            var last = Children.LastOrDefault();
+            if (last == null)
+            {
+                return new SizeRequest(Size.Zero);
+            }
+            switch (Orientation)
+            {
+                case StackOrientation.Horizontal:
+                    throw new NotImplementedException();
+
+                case StackOrientation.Vertical:
+                    return new SizeRequest(new Size
+                    {
+                        Width = widthConstraint,
+                        Height = last.Bounds.Bottom
+                    });
+
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        private BitArray CreateKey(params double[] values)
+        {
+            return new BitArray(BitConverter.GetBytes((int)Orientation)
+                                            .Concat(values.SelectMany(BitConverter.GetBytes))
+                                            .ToArray());
+        }
+
+        protected override void LayoutChildren(double x, double y, double width, double height)
+        {
+            EnsureLayout(new Rectangle
+                         {
+                             Location = new Point(x, y),
+                             Size = new Size
+                                    {
+                                        Width = width,
+                                        Height = height
+                                    }
+                         });
         }
     }
 }
